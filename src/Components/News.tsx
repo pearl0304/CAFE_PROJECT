49548b218ad1581c22c9acfa3af49c7a6d010985
Kylie <kylie@comets.kr>
2022-08-05 16:29:41 +0900

    feat: News component

--- src/Components/News.tsx
+++ src/Components/News.tsx
@@ -1,6 +1,35 @@
 import React from "react";
 import { NewsWrapper } from "../css/News.styled";
+import news_01 from "../asserts/news_01.jpg";
+import news_02 from "../asserts/news_02.jpg";
 
 export const News = () => {
-  return <NewsWrapper id="home"></NewsWrapper>;
+  return (
+    <NewsWrapper id="home">
+      <div className="news-container">
+        <div className="news-left">
+          <div className="line-box_1"></div>
+          <div className="line-box_2"></div>
+          <div className="news-left-image">
+            <img src={news_01}></img>
+          </div>
+          <div className="news-title">
+            <span>SUMMER</span>
+            <span>PROMOTION</span>
+          </div>
+          <div>
+            <span>it so hot </span>
+          </div>
+        </div>
+        <div className="news-right">
+          <div>
+            <span>Check in. Chew up. Tune out. Ok Drugs helps elevate micro-experiences for those who want to check-in to the moment in a hard and heavy world.</span>
+          </div>
+          <div className="news-right-image">
+            <img src={news_02}></img>
+          </div>
+        </div>
+      </div>
+    </NewsWrapper>
+  );
 };
--- src/css/News.styled.ts
+++ src/css/News.styled.ts
@@ -3,4 +3,86 @@ import styled from "styled-components";
 export const NewsWrapper = styled.div`
   background-color: #fbb04c;
   height: 50rem;
+
+  .news-container {
+    display: flex;
+    justify-content: center;
+    gap: 10rem;
+    width: 90%;
+  }
+
+  .news-left {
+    margin: 10rem 5rem;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  .news-left-image {
+    width: 15rem;
+    height: 15rem;
+  }
+
+  .news-left-image img {
+    width: 100%;
+    height: 100%;
+    border-radius: 1rem;
+  }
+
+  .news-left .news-title {
+    display: flex;
+    flex-direction: column;
+    margin-top: 3rem;
+  }
+
+  .news-left .news-title span {
+    font-size: 5rem;
+    font-weight: 900;
+    color: #31302f;
+    font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+    letter-spacing: 0.5rem;
+  }
+
+  .news-right {
+    margin-top: 10rem;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    align-items: flex-end;
+  }
+
+  .news-right > div:first-child {
+    width: 20rem;
+    font-size: 14px;
+  }
+
+  .news-right-image {
+    width: 20rem;
+    height: 25rem;
+  }
+  .news-right-image img {
+    width: 100%;
+    height: 100%;
+    border-radius: 1rem;
+  }
+
+  .line-box_1 {
+    position: absolute;
+    width: 15rem;
+    height: 15rem;
+    border: 2px solid #ff8573;
+    top: -1rem;
+    left: -1rem;
+  }
+
+  .line-box_2 {
+    position: absolute;
+    width: 15rem;
+    height: 15rem;
+    border: 2px solid #ff8;
+    top: 1rem;
+    left: 1rem;
+  }
 `;
