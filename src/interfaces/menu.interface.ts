a33da3e695cc3382fa349e7b8f40940b49101661
kylie <wlswn4630@gmail.com>
2022-08-08 21:37:50 +0900

    set interface

--- src/interfaces/menu.interface.ts
+++ src/interfaces/menu.interface.ts
@@ -1,1 +1,19 @@
-export const menuInterface = {};
+export interface MenuListInterface {
+  id: string;
+  title: string;
+  subTitle: string;
+  image: string;
+  description: string;
+  nutrients: string;
+  type: string;
+  price: number;
+}
+
+export interface CartListInterface {
+  id: string;
+  title: string;
+  subTitle: string;
+  image: string;
+  price: number;
+  amount: number;
+}
