1dc7098bd5de2802b5ad4ead605acc2b3d8e510a
Kylie <kylie@comets.kr>
2022-08-10 14:23:12 +0900

    style: DetailList

--- src/Components/DetailList.tsx
+++ src/Components/DetailList.tsx
@@ -1,15 +1,41 @@
 import { FC } from "react";
 import { DetailListWrapper } from "../css/DetailList.styled";
 import { MenuListInterface } from "../interfaces/menu.interface";
+import Button from "@mui/material/Button";
 
 type Props = {
   menu: MenuListInterface;
 };
+
+const numberFormat = (price: number): string => {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
 export const DetailList: FC<Props> = ({ menu }) => {
-  console.log(menu.title);
   return (
     <DetailListWrapper>
-      <div>{menu.title}</div>
+      <div className="detail-container">
+        <div className="detail-left">
+          <div>
+            <img src={menu.image} alt={menu.title}></img>
+          </div>
+          <div className="nutrients-info">
+            <span>영양 성분</span>
+            <span>{menu.nutrients}</span>
+          </div>
+        </div>
+        <div className="detail-right">
+          <div className="menu-detail-info">
+            <h1>{menu.title}</h1>
+            <h2> - {menu.subTitle}</h2>
+            <span>{menu.description}</span>
+            <span>{numberFormat(menu.price)}원</span>
+          </div>
+          <div className="cart-btn">
+            <Button>장바구니에 담기</Button>
+          </div>
+        </div>
+      </div>
     </DetailListWrapper>
   );
 };
--- src/css/DetailList.styled.ts
+++ src/css/DetailList.styled.ts
@@ -2,4 +2,41 @@ import styled from "styled-components";
 
 export const DetailListWrapper = styled.div`
   margin-top: 4.5rem;
+
+  .detail-container {
+    padding-top: 6rem;
+    width: 90%;
+    display: flex;
+    justify-content: center;
+    gap: 5rem;
+    margin-left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .detail-left {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-bottom: 1.5rem;
+  }
+
+  .nutrients-info {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  .detail-right {
+    width: 480px;
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  .detail-right .menu-detail-info {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
 `;
